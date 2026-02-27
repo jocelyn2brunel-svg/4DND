5cba55edb26e18b69fe295348562c4bda0c1fe64
Jocelyn <jocelyn2brunel@gmail.com>
2026-02-27 09:52:55 -0500

    Ajout des noms pour la race Drakéide dans Race.cs
    Ajout des listes de prénoms masculins, prénoms féminins et noms de clan pour la race Drakéide, avec des exemples pour chaque catégorie afin d’enrichir les informations de cette race.

--- Race.cs
+++ Race.cs
@@ -322,6 +322,9 @@ public class Race
             HasBreathWeapon = true,
             HasDamageResistance = true,
             Languages = new List<string> { "Common", "Draconic" },
+            MaleNames = new List<string> { "Arjhan", "Balasar", "Bharash", "Donaar", "Ghesh", "Heskan", "Kriv", "Medrash", "Mehen", "Nadarr", "Pandjed", "Patrin", "Rhogar", "Shamash", "Shedinn", "Tarhun", "Torinn" },
+            FemaleNames = new List<string> { "Akra", "Biri", "Daar", "Farideh", "Harann", "Havilar", "Jheri", "Kava", "Korinn", "Mishann", "Nala", "Perra", "Raiann", "Sora", "Surina", "Thava", "Uadjit" },
+            ClanNames = new List<string> { "Clethtinthiallor", "Daardendrian", "Delmirev", "Drachedandion", "Fenkenkabradon", "Kepeshkmolik", "Kerrhylon", "Kimbatuul", "Linxakasendalor", "Myastan", "Nemmonis", "Norixius", "Ophinshtalajiir", "Prexijandilin", "Shestendeliath", "Turnuroth", "Verthisathurgiesh", "Yarjerit" },
             Description = Loc.Tr("Draconic heritage (+2 STR, +1 CHA, Draconic Ancestry, Breath Weapon, Damage Resistance)")
         },
         
